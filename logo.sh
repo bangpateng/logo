3ea1ddbd87004e82aa6af4c19a703cb65f2d8972
Bang Pateng <38981255+bangpateng@users.noreply.github.com>
2025-02-20 13:17:42 +0700

    Create logo.sh

---
+++ logo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+BLUE='\033[1;34m'
+NC='\033[0m'
+
+echo -e "${BLUE}"
+echo "░█▀▄░█▀█░█▀█░█▀▀  ░█▀█░█▀█░▀█▀░█▀▀░█▀█░█▀▀"
+echo "░█▀▄░█▀█░█░█░█░█  ░█▀▀░█▀█░░█░░█▀▀░█░█░█░█"
+echo "░▀▀░░▀░▀░▀░▀░▀▀▀  ░▀░░░▀░▀░░▀░░▀▀▀░▀░▀░▀▀▀"
+echo ""
+echo "Follow Telegram : @bangpateng_airdrop"
+echo "Website :https://bangpateng.xyz"
+echo -e "${NC}"
